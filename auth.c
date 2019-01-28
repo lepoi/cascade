e42ae699bc4be11fd121c5cc5c9c84072557ae3c
lepoi <terminallyquack@gmail.com>
2019-01-28 11:22:54 -0800

    Added base auth files

---
+++ auth.c
@@ -0,0 +1,17 @@
+// [1] receive client's username and password
+
+// if IP in file :
+
+// [1.1] process message as command
+
+// else
+
+// [1.2] process message as login
+
+// if login is well-formatted
+
+// [1.2.1] create entry in clients file and return success message
+
+// else
+
+// [1.2.2] return error message
