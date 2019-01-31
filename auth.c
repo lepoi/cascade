6c46050a47482e5cd851a26ae871d37135024ecf
lepoi <terminallyquack@gmail.com>
2019-01-30 17:12:10 -0800

    Added database functions

--- auth.c
+++ auth.c
@@ -1,17 +1,98 @@
-// [1] receive client's username and password
+char *DB = "clients";
+const size_t USRLEN = 8, PASSLEN = 8, ADDRLEN = 15;
+size_t CLIENTREGLEN = USRLEN + PASSLEN + 1, SESSIONREGLEN = USRLEN + ADDRLEN + 1;
 
-// if IP in file :
+char lookup_user(char *username, char *password) {
+	FILE *db = fopen(DB, "r");
+	char *buffer;
 
-// [1.1] process message as command
+	if (!db) {
+		printf("Error opening user database");
+		return 1;
+	}
 
-// else
+	char *user = calloc(15, 1), *pass;
 
-// [1.2] process message as login
+	if (password == NULL) {		
+		while(getline(&buffer, &CLIENTREGLEN, db) != -1) {
+			sscanf(buffer, "%s;", user);
+			// printf("[user] \"%s\"\n", user);
+			if (strcmp(username, user) == 0)
+				return 0;
+		}
+	}
+	else {
+		pass = calloc(8, 1);
+		while(getline(&buffer, &CLIENTREGLEN, db) != -1) {
+			sscanf(buffer, "%s;%s", user, pass);
+			// printf("[user] \"%s\", [pass] \"%s\"\n", user, pass);
+			if (strcmp(username, user) == 0 && strcmp(password, pass) == 0)
+				return 0;
+		}
+	}
 
-// if login is well-formatted
+	fclose(db);
+	return 1;
+}
 
-// [1.2.1] create entry in clients file and return success message
+char lookup_session(char *username, char *address) {
+	FILE *db = fopen(DB, "r");
+	char *buffer;
 
-// else
+	if (!db) {
+		printf("Error opening user database");
+		return 1;
+	}
 
-// [1.2.2] return error message
+	char *user = calloc(15, 1), *addr = calloc(8, 1);
+	while(getline(&buffer, &SESSIONREGLEN, db) != -1) {
+		sscanf(buffer, "%s;%s", user, addr);
+		// printf("[user] \"%s\", [addr] \"%s\"\n", user, addr);
+		if (strcmp(username, user) == 0 && strcmp(address, addr) == 0)
+			return 0;
+
+	}
+
+	fclose(db);
+
+	return 1;
+}
+
+char add_user(char *username, char *password) {
+	return 0;
+}
+
+char rm_user() {
+	return 0;
+}
+
+char add_session() {
+	return 0;
+}
+
+char rm_session() {
+	return 0;
+}
+
+char login(char *buffer, char *address) {
+	char *user, *pass, error;
+	sscanf(buffer, "%s;%s", user, pass);
+
+	if (error = lookup_user(user, pass))
+		return error;
+
+	rm_session(address);
+	add_session(user, address);
+}
+
+char login_session(char *username, char *address) {
+	return lookup_session(username, address);
+}
+
+char logout(char *address) {
+	return rm_session(address);
+}
+
+char signup(char *username) {
+	return lookup_user(username, NULL);
+}
--- socket_test.c
+++ socket_test.c
@@ -3,25 +3,77 @@
 
 #include <netdb.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
 
-void doprocessing (int sock);
+#include "auth.c"
 
-void doprocessing (int sock) {
-   int n;
+// mostly authorization stuff
+char dopreprocessing(char *client_addr, char *buffer) {
+   char operation = buffer[0], *body = &buffer[2], error;
+
+   // if (strcmp(client_addr, "127.0.0.1") != 0)
+   //    return 1;
+
+   // is it a broadcast attempt?
+   if (operation == 'B') {
+
+      goto SUCCESS;
+   }
+
+   // is it a login attempt?
+   // check if username and password match
+   if (operation == 'I') {
+      if (error = login(body, client_addr))
+         return error;
+   }
+   
+   // is it a logout attempt?
+   // look for a session and end it
+   if (operation == 'O')
+      return logout(client_addr);
+   
+   // is it a login attempt using session?
+   // check if username and client_addr match
+   if (operation == 'S') {
+      return login_session(body, client_addr);
+   }
+   
+   // is it a registration attempt?
+   // if user doesn't exist, create record
+   //    TODO: store username
+   //    TODO: store password
+   // register_user(client_addr);
+   if (operation == 'R') {
+      if (signup(body))
+         return 1;      
+   }
+
+   SUCCESS:
+
+   return 0;
+}
+
+void doprocessing(int sock, char *client_addr) {
+   int n, error;
    char buffer[256];
-   bzero(buffer,256);
-   n = read(sock,buffer,255);
+   bzero(buffer, 256);
+   n = read(sock, buffer, 255);
    
    if (n < 0) {
       perror("ERROR reading from socket");
       exit(1);
    }
+
+   if (error = dopreprocessing(client_addr, buffer)) {
+      printf("[ %s ] [ %s ] error: %u", buffer[0], client_addr, error);
+      exit(1);
+   }
    
    printf("Message received: %s", buffer);
 
-   n = write(sock,"Echo: ", 6);
+   n = write(sock, "Echo: ", 6);
    if (n < 0) {
       perror("ERROR writing to socket");
       exit(1);
@@ -34,9 +86,9 @@ void doprocessing (int sock) {
    }
 }
 
-int main( int argc, char *argv[] ) {
+int main(int argc, char *argv[]) {
    int sockfd, newsockfd, portno, clilen;
-   char buffer[256];
+   char buffer[256], client_addr[INET_ADDRSTRLEN];
    struct sockaddr_in serv_addr, cli_addr;
    int n, pid;
    
@@ -49,7 +101,7 @@ int main( int argc, char *argv[] ) {
    }
 
    int true = 1;
-   setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &true,sizeof(int));
+   setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &true, sizeof(int));
    
    /* Initialize socket structure */
    bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -80,6 +132,9 @@ int main( int argc, char *argv[] ) {
          perror("ERROR on accept");
          exit(1);
       }
+
+      // isolate client's ip address
+      strcpy(inet_ntoa(cli_addr.sin_addr), client_addr);
       
       /* Create child process */
       pid = fork();
@@ -92,7 +147,7 @@ int main( int argc, char *argv[] ) {
       if (pid == 0) {
          /* This is the client process */
          close(sockfd);
-         doprocessing(newsockfd);
+         doprocessing(newsockfd, client_addr);
          exit(0);
       }
       else {
